9719c0b263982fd686e166702f46eb7009b5d5b3
rodriguezh21 <rodriguezh21@gmail.com>
2022-03-10 09:42:53 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//:rodriguezh21.github.io/portfolio/precache-manifest.b05b72d20b15def21c90319453a32b01.js"
+  "/https//rodriguezh21.github.io/portfolio/precache-manifest.872031004d301f99ca603b71681cecae.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//:rodriguezh21.github.io/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//rodriguezh21.github.io/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
