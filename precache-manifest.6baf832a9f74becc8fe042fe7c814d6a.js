59e7a05df48b34bf708d404dd454303768b43cba
rodriguezh21 <rodriguezh21@gmail.com>
2022-03-12 20:29:38 -0600

    Updates

---
+++ precache-manifest.6baf832a9f74becc8fe042fe7c814d6a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a59386f154ab983f2160942a830e4c18",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "3a1beaf2d5592e703fc5",
+    "url": "/portfolio/static/css/2.28a45c22.chunk.css"
+  },
+  {
+    "revision": "1ae878528a1e901bb0ff",
+    "url": "/portfolio/static/css/main.6807a0de.chunk.css"
+  },
+  {
+    "revision": "3a1beaf2d5592e703fc5",
+    "url": "/portfolio/static/js/2.e5daf84e.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/portfolio/static/js/2.e5daf84e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ae878528a1e901bb0ff",
+    "url": "/portfolio/static/js/main.59f18457.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "20103f6550c698db40e39593e63120e8",
+    "url": "/portfolio/static/media/stacked-waves.20103f65.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/https//rodriguezh21.github.io/portfolio/precache-manifest.e090288a388ed8f166f6f85a184a7520.js"
+  "/portfolio/precache-manifest.6baf832a9f74becc8fe042fe7c814d6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/https//rodriguezh21.github.io/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
